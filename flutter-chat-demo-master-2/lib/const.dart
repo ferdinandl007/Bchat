4cc83615ad6061ece7c4ca6f12462789a79f5a82
ferdinandl007 <ferdinandloesch@me.com>
2018-12-07 15:46:46 +0000

    up

---
+++ flutter-chat-demo-master-2/lib/const.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+final themeColor = new Color(0xffd10173);
+final primaryColor = new Color(0xff203152);
+final greyColor = new Color(0xffbd0169);
+final greyColor2 = new Color(0xffbd0169);
